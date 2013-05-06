60371a9b9b5e17b2027c21d76a282aa74109227c
tokkonopapa <tokkonopapa@yahoo.com>
2013-05-06 16:29:48 +0900

    first commit

---
+++ ajax-with-nonce/js/script.js
@@ -0,0 +1,21 @@
+(function ($) {
+	$(function () {
+		function my_ajax_call( id ) {
+			// `MyAjaxSample` is enqueued by wp_localize_script()
+			$.when( $.post( MyAjaxSample.ajax_url,
+				{ action: MyAjaxSample.action, ajax_nonce: MyAjaxSample.ajax_nonce } )
+			// response: 0 (no handler) / -1 (invalid nonce)
+			).done( function( data, textStatus, jqXHR ) {
+				$( id ).text( '0' !== data ? data : 'deactivated' );
+			}).fail( function( jqXHR, textStatus, errorThrown ) {
+				$( id ).text( jqXHR.responseText );
+			});
+		}
+
+		// Preload now button
+		$( '#my_ajax_sample' ).on( 'click', function() {
+			$( '#my_ajax_response' ).text( "Requesting ..." );
+			my_ajax_call( '#my_ajax_response' );
+		});
+	});
+}(jQuery));
---
+++ ajax-with-nonce/plugin.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Plugin Name: My Ajax Sample
+Plugin URI: https://github.com/tokkonopapa/WordPress-Plugin-Snipets
+Description: A ajax sample using nonce for WordPress.
+Version: 0.1
+Author: tokkonopapa
+Author URI: https://github.com/tokkonopapa
+Author Email: 
+License: GPLv2 or later
+*/
+
+if( ! class_exists( 'MyAjaxSample' ) ) :
+
+class MyAjaxSample {
+	// Constants
+	const PLUGIN_SLUG = 'my_ajax';
+
+	// Constructor
+	function __construct() {
+		// Register scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
+
+		// Register shortcode
+		add_shortcode( self::PLUGIN_SLUG, array( $this, 'enqueue_shortcode' ) );
+
+		// Register ajax callback for logged in and not logged in user
+		$action = $this->get_anction_name();
+		add_action( 'wp_ajax_' . $action, array( $this, 'ajax_callback' ) );
+		add_action( 'wp_ajax_nopriv_' . $action, array( $this, 'ajax_callback' ) );
+	}
+
+	// Registers and enqueues JavaScript
+	private function register_scripts() {
+		$handle = self::PLUGIN_SLUG . '-script';
+		wp_enqueue_script( $handle, plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ) );
+
+		$action = $this->get_anction_name();
+		wp_localize_script( $handle, 'MyAjaxSample', array(
+			'action' => $action,
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
+			'ajax_nonce' => wp_create_nonce( $action ),
+		) );
+	}
+
+	// Registers and enqueues plugin-specific scripts
+	public function register_plugin_scripts() {
+		$this->register_scripts();
+	}
+
+	// Shortcode for '[my_ajax]'
+	public function enqueue_shortcode( $atts ) {
+		extract( shortcode_atts( array(
+			'text' => 'click me',
+		), $atts ) );
+
+		$text = sanitize_text_field( $text );
+		$code = '<input type="button" id="my_ajax_sample" value="' . $text . '" />';
+		$code .= '<p id="my_ajax_response"></p>';
+		return $code;
+	}
+
+	// Get the name of ajax action
+	private function get_anction_name() {
+		return self::PLUGIN_SLUG . '-action';
+	}
+
+	// Callback for ajax
+	public function ajax_callback() {
+//		if ( check_ajax_referer( $this->get_anction_name(), 'ajax_nonce', false ) ) {
+		if ( wp_verify_nonce( $_REQUEST['ajax_nonce'], $this->get_anction_name() ) ) {
+//			if ( is_user_logged_in() ) {
+			if ( current_user_can( 'edit_posts' ) ) {
+				echo 'hello, registered user!';
+			} else {
+				echo 'hello, visitor!';
+			}
+		} else {
+			header('HTTP/1.1 403 Forbidden');
+			echo 'You don\'t have right permission.';
+		}
+		die();
+	}
+
+} // end class
+
+global $my_ajax_sample;
+$my_ajax_sample = new MyAjaxSample();
+
+endif; // class_exists
