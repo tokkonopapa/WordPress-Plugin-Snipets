050aa3ec7e0802dedd1662d02efc821296b3f8eb
tokkonopapa <tokkonopapa@yahoo.com>
2013-05-11 02:14:34 +0900

    parse jslint and tied up

--- ajax-with-nonce/js/script.js
+++ ajax-with-nonce/js/script.js
@@ -1,23 +1,31 @@
 (function ($) {
-	$(function () {
-		function my_ajax_request( id ) {
-			// `MyAjax` is enqueued by wp_localize_script()
-			$.post( MyAjax.url, {
-				action: MyAjax.action,
-				token: MyAjax.token
-			}).done( function( data, textStatus, jqXHR ) {
-				// if no hander, response may be '0'
-				$( id ).text( '0' !== data ? data.message : 'deactivated' );
-			}).fail( function( jqXHR, textStatus, errorThrown ) {
-				// if nonce does't match, response may be '-1'
-				$( id ).text( jqXHR.responseText );
-			});
-		}
+	function my_ajax_request(id) {
+		// `MyAjax` is enqueued by wp_localize_script()
+		$.post(MyAjax.url, {
+			action: MyAjax.action,
+			token: MyAjax.token
+		}).done(function (data, textStatus, jqXHR) {
+			// if no hander, response may be '0'
+			if ('0' !== data) {
+				var msg = '';
+				$.each(data, function (key, val) {
+					msg += key + " => " + val + "<br />";
+				});
+				$(id).text(msg);
+			} else {
+				$(id).text('deactivated');
+			}
+		}).fail(function (jqXHR, textStatus, errorThrown) {
+			// if nonce does't match, response may be '-1'
+			$(id).text(jqXHR.responseText);
+		});
+	}
 
-		// Preload now button
-		$( '#my_ajax_sample' ).on( 'click', function() {
-			$( '#my_ajax_response' ).text( "Requesting ..." );
-			my_ajax_request( '#my_ajax_response' );
+	$(function () {
+		// Ajax button
+		$('#my_ajax_sample').on('click', function () {
+			$('#my_ajax_response').text("Requesting ...");
+			my_ajax_request('#my_ajax_response');
 		});
 	});
 }(jQuery));
